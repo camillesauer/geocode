f4fa03429258af30016720c3cc0943f4977b6909
Camille Sauer <camille-sauer@hotmail.fr>
2020-12-08 15:48:03 +0100

    accueil ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :trajets , only: [:index, :new, :create, :show, :update, :edit, :destroy]
+  resources :poetes, only: [:new, :create, :show, :update, :edit, :destroy]
+  resources :actions, only: [:index]
+
+  get    '/mentions'      => 'pages#mentions'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
