0ea8494251e1a1dd51a383cae399634a60ce1328
Camille Sauer <camille-sauer@hotmail.fr>
2020-12-09 15:15:10 +0100

    journal created - show poete et trajet ok

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :actions, only: [:index]
 
   get    '/mentions'      => 'pages#mentions'
+  get    '/journal'      => 'pages#journal'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
